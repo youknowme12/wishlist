fbe16eaad3ef9f9fbbd5281668d30f54bb1afeec
Ayushi Jyoti <ayushijyoti75@gmail.com>
2024-02-20 01:32:43 +0530

    Refactored code

--- wishlist/src/main/java/com/backend/wishlist/controller/WishlistController.java
+++ wishlist/src/main/java/com/backend/wishlist/controller/WishlistController.java
@@ -3,13 +3,11 @@ package com.backend.wishlist.controller;
 import com.backend.wishlist.entity.Wishlist;
 import com.backend.wishlist.service.WishlistService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 @RestController
 public class WishlistController {
@@ -17,19 +15,17 @@ public class WishlistController {
     @Autowired
     private WishlistService wishlistService;
 
-    private final Logger logger = (Logger) LoggerFactory.getLogger(getClass());
-
     // to get csrf token
     @GetMapping("/csrf-token")
     public CsrfToken retrieveCsrfToken(HttpServletRequest request) {
-        logger.info("The csrf token is obtained");
+
         return (CsrfToken) request.getAttribute("_csrf");
     }
 
     // for testing basic authorization
     @GetMapping("/hello-world")
     public String helloWorld(){
-        logger.info("Hello World. Testing Successful");
+
         return "Hello World";
     }
 
@@ -37,7 +33,6 @@ public class WishlistController {
     @GetMapping("/api/wishlists")
     public List<Wishlist> getAllProducts(){
 
-        logger.info("Request received for getting all the records from wishlist entity");
         return wishlistService.fetchAllProduct();
     }
 
@@ -45,7 +40,6 @@ public class WishlistController {
     @PostMapping("/api/wishlists")
     public Wishlist saveProduct(@RequestBody Wishlist wishlist){
 
-        logger.info("Request received for saving product");
         return wishlistService.saveProduct(wishlist);
     }
 
@@ -53,7 +47,6 @@ public class WishlistController {
     @DeleteMapping("/api/wishlists/{id}")
     public String deleteProductById(@PathVariable("id") Integer id){
 
-        logger.info("Request received for deleting a product by id");
         return wishlistService.deleteProductById(id);
     }
 }
--- wishlist/src/main/java/com/backend/wishlist/service/WishlistServiceImpl.java
+++ wishlist/src/main/java/com/backend/wishlist/service/WishlistServiceImpl.java
@@ -2,26 +2,24 @@ package com.backend.wishlist.service;
 
 import com.backend.wishlist.entity.Wishlist;
 import com.backend.wishlist.repository.WishlistRepository;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 @Service
+@Slf4j
 public class WishlistServiceImpl implements WishlistService{
 
     @Autowired
     private WishlistRepository wishlistRepository;
 
-    private final Logger logger = (Logger) LoggerFactory.getLogger(getClass());
 
     // save product in wishlist
     @Override
     public Wishlist saveProduct(Wishlist wishlist) {
 
-        logger.info("Added new product in the wishlist");
         return wishlistRepository.save(wishlist);
     }
 
@@ -29,15 +27,12 @@ public class WishlistServiceImpl implements WishlistService{
     @Override
     public List<Wishlist> fetchAllProduct(){
 
-        logger.info("Found all the products present in the wishlist");
         return wishlistRepository.findAll();
     }
 
     // delete a product by id present in the wishlist
     @Override
     public String deleteProductById(Integer Id){
-
-        logger.info("Deleted product from wishlist");
 
         if(wishlistRepository.findById(Id).isPresent()){
             wishlistRepository.deleteById(Id);
